b344583bd38a69fa6d31908107ab13a0155efb68
André Santos Castelo <andrecasteloecomp@gmail.com>
2021-02-02 01:35:57 -0300

    Solvingo size error

--- main.c
+++ main.c
@@ -4,25 +4,23 @@
 int main(){
 
     int isDigraph = 0;
-    Graphs* graphs = createGraph(7, 650, 1);
+    Graphs* graphs = createGraph(7, 500, 0);
 
-    insertsEdge(graphs, 0, 5, isDigraph, 200);
-    insertsEdge(graphs, 0, 3, isDigraph, 650);
+    insertsEdge(graphs, 0, 5, isDigraph, 100);
+    insertsEdge(graphs, 0, 3, isDigraph, 700);
 
-    insertsEdge(graphs, 1, 3, isDigraph, 350);
-    insertsEdge(graphs, 1, 2, isDigraph, 250);
+    insertsEdge(graphs, 1, 3, isDigraph, 250);
+    insertsEdge(graphs, 1, 2, isDigraph, 200);
 
-    insertsEdge(graphs, 3, 4, isDigraph, 300);
-    insertsEdge(graphs, 3, 5, isDigraph, 400);
-    insertsEdge(graphs, 3, 2, isDigraph, 600);
+    insertsEdge(graphs, 3, 4, isDigraph, 250);
+    insertsEdge(graphs, 3, 2, isDigraph, 300);
+    insertsEdge(graphs, 5, 3, isDigraph, 450);
 
-    insertsEdge(graphs, 6, 5, isDigraph, 280);
+    insertsEdge(graphs, 6, 5, isDigraph, 300);
 
-
-
-    int i, father[6];
+    int i, father[7];
     Kruskal(graphs, 0, father);
-    for(i = 0; i < 6; i++){
+    for(i = 0; i < 7; i++){
         printf("%d: %d\n", father[i], i);
      }
     printf("\n");
